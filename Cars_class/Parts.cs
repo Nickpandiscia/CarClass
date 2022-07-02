58ff5bbc3ad40025d177d1729793c0ed961dbd76
Nickpandiscia <npandiscia1@gmail.com>
2022-07-01 21:54:11 -0400

    initial commit

---
+++ Cars_class/Parts.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_class
+{
+    public class Parts
+    {
+        public string Name { get; set; }
+        public string Engine { get; set; }
+        public int Horsepower { get; set; }
+        public string Tires { get; set; }
+        public string Make  { get; set; }
+        public bool Fourwheeldrive { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public int Milespergallon { get; set; }
+
+        public void Driving(string theDrive, string thePlace)
+        {
+            Console.WriteLine($"{theDrive} was great in the Chevy Malibu! we drove to {thePlace}.");
+        }
+
+
+    }
+}
---
+++ Cars_class/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cars_class
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {    
+            var myCar = new Parts();
+            myCar.Name = "Classic";
+            myCar.Engine = " Four cyliner";
+            myCar.Horsepower = 180;
+            myCar.Tires = "Goodyears";
+            myCar.Make = "Chevy";
+            myCar.Fourwheeldrive = false;
+            myCar.Model = "Malibu";
+            myCar.Milespergallon = 30;
+            myCar.Year = 2004;
+
+            var carList = new List<Parts>();
+            carList.Add(myCar);
+
+            foreach(var part in carList)
+            {
+                Console.WriteLine(part.Name + " " + part.Engine + " " + part.Horsepower);
+                Console.WriteLine(part.Make + " " + part.Tires + " " + part.Fourwheeldrive + " " + part.Model);
+                Console.WriteLine(part.Milespergallon+ " " + part.Year);               
+            }
+
+            myCar.Driving("Highway", "Scranton");
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
